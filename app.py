32b33d07df392f68e973c4c4d9c8c67f3cf5a146
Avila <guilherme.medeiros.avila@gmail.com>
2024-05-21 19:34:37 -0300

    creating user in database

--- app.py
+++ app.py
@@ -7,28 +7,28 @@ app.secret_key = "sua_chave_secreta_aqui"  # Defina sua chave secreta aqui
 
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-# SQL ALCHEMY COINFIG
+# SQL ALCHEMY CONFIG
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/bibliomania'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class users(db.Model):
+class User(db.Model):
     _id = db.Column("id", db.Integer, primary_key = True)
     name = db.Column("name", db.String(100))
     email = db.Column("email", db.String(100))
     cpf = db.Column("cpf", db.String(12))
     password = db.Column("password", db.String(100))
     user_type = db.Column("user_type", db.String(100))
+    phonenumber = db.Column("phonenumber", db.String(100))
 
-    def __init__(self, name, email, cpf, password):
-        default_role = "leitor"
-
+    def __init__(self, name, email, cpf, password, default_role, phonenumber):
         self.name = name
         self.email = email
         self.cpf = cpf
         self.password = password
         self.user_type = default_role
+        self.phonenumber = phonenumber
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -47,18 +47,34 @@ def login():
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
-        # Recuperando os dados do formulário
         nome = request.form["input_nome"]
         cpf = request.form["input_cpf"]
         email = request.form["input_email"]
         password = request.form["input_password"]
-        telefone = request.form["input_telefone"]
-        # Salvar os dados no banco de dados ou em outro local adequado
+        phonenumber = request.form["input_telefone"]
 
-        # Redirecionar para a página do usuário após o cadastro
-        return redirect(url_for("user"))
+        if not nome or not cpf or not email or not password or not phonenumber:
+            flash("Por favor, preencha todos os campos.", "error")
+            return redirect(url_for("signup"))
+
+        found_user_email = User.query.filter_by(email=email).first()
+        found_user_cpf = User.query.filter_by(cpf=cpf).first()
+
+        if found_user_email or found_user_cpf:
+            flash("Email ou cpf já cadastrado!", "error")
+            return redirect(url_for("signup"))
+
+        else:
+            new_user = User(nome, email, cpf, password, "user", phonenumber)
+            db.session.add(new_user)
+            db.session.commit()
+            session["email"] = email
+            session["password"] = password
+            flash("Usuário cadastrado com sucesso!", "success")
+            return redirect(url_for("login"))
+
+    # Se o usuário já estiver logado
     else:
-        # Verificar se o usuário já está logado
         if "email" in session and "password" in session:
             return redirect(url_for("user"))
 
