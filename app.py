b8770ccff74bf982976ba8a51d87c20a5d97882b
Avila <guilherme.medeiros.avila@gmail.com>
2024-05-24 14:00:44 -0300

    feat: login required and already logged decorator

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 from flask import redirect, url_for, render_template, request, session, flash
+from functools import wraps
 from app.utils.validations import validate_email, validate_cpf
 from app.models.User import User
 from app import app, db
 
-"""
-ROTAS DO USER
-"""
-# TODO:
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "email" not in session:
+            return redirect(url_for("login"))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def already_logged_in(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "email" in session:
+            return redirect(url_for("home"))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route('/')
+@login_required
+def home():
+    return redirect(url_for("user"))
+
+
 @app.route("/login", methods=["POST", "GET"])
+@already_logged_in
 def login():
     if request.method == "POST":
         email = request.form["input_email"]
@@ -18,27 +41,19 @@ def login():
         session.pop("password", None)
 
         found_user = User.query.filter_by(email=email).first()
-        if found_user:
-            if found_user.check_password(password):
-                session["email"] = email
-                flash("Login realizado com sucesso!", "success")
-                return redirect(url_for("user"))
-            else:
-                flash("Email ou senha inválidos!", "error")
-                return redirect(url_for("login"))
+        if found_user and found_user.check_password(password):
+            session["email"] = email
+            flash("Login realizado com sucesso!", "success")
+            return redirect(url_for("user"))
         else:
             flash("Email ou senha inválidos!", "error")
             return redirect(url_for("login"))
 
+    return render_template("sign-in-template.html")
 
-        return redirect(url_for("user"))
-    else:
-        if "email" in session:
-            return redirect(url_for("user"))
-
-        return render_template("sign-in-template.html")
 
 @app.route("/signup", methods=["POST", "GET"])
+@already_logged_in
 def signup():
     if request.method == "POST":
         nome = request.form["input_nome"]
@@ -59,10 +74,7 @@ def signup():
             flash("CPF inválido.", "error")
             return redirect(url_for("signup"))
 
-        found_user_email = User.query.filter_by(email=email).first()
-        found_user_cpf = User.query.filter_by(cpf=cpf).first()
-
-        if found_user_email or found_user_cpf:
+        if User.query.filter_by(email=email).first() or User.query.filter_by(cpf=cpf).first():
             flash("Email ou cpf já cadastrado!", "error")
             return redirect(url_for("signup"))
 
@@ -73,31 +85,21 @@ def signup():
             session["email"] = email
             flash("Usuário cadastrado com sucesso!", "success")
             return redirect(url_for("login"))
-
         except Exception as e:
             flash("Erro ao cadastrar o usuário.", "error")
             app.logger.error(f"Erro ao cadastrar usuário: {e}")
             return redirect(url_for("signup"))
 
-    # TODO: tratar caso o usuário tenha session
-    # Se o usuário já estiver logado
-    else:
-        if "email" in session:
-            return redirect(url_for("user"))
+    return render_template("sign-up-template.html")
 
-        # Se não estiver logado, renderizar o template de cadastro
-        return render_template("sign-up-template.html")
 
-# TODO:
 @app.route("/user")
+@login_required
 def user():
-    if "email" in session:
-        email = session["email"]
-        return f"<h1>Session log</h1><p>User: {email}</p>"
-    else:
-        return redirect(url_for("login"))
+    email = session["email"]
+    return f"<h1>Session log</h1><p>User: {email}</p>"
 
-# TODO:
+
 @app.route("/logout")
 def logout():
     session.clear()
@@ -107,7 +109,6 @@ def logout():
 """
 ROTAS BOOK
 """
-
 
 """
 CONFIG
