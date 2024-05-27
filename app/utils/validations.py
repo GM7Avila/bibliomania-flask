7a7400e4da0f15e960c27808a2e4c3e540b5a526
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-05-26 22:00:05 -0300

    fix

--- app/utils/validations.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def validate_email(email):
-    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return re.match(email_regex, email)
-
-def validate_cpf(cpf):
-    cpf_regex = r'^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$'
-    return re.match(cpf_regex, cpf)
