a10e5dc2d7b4b618768a9c6865152aefc2979747
Machado <luucas.machadoo@gmail.com>
2024-06-07 21:59:55 -0300

    doc: comentario no arquivo explicando o  modal_change_password.js

--- app/static/js/modal_change_password.js
+++ app/static/js/modal_change_password.js
@@ -1,3 +1,4 @@
+// submit ao evento de click do button change-password-form
 document.addEventListener("DOMContentLoaded", function() {
     const confirmPasswordButton = document.getElementById('confirm-password');
     const changePasswordForm = document.getElementById('change-password-form');
